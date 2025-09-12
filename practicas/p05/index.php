b4127d46a4e9be3536d2f6c1de97784ff0a7392a
DarvoDev <davidcarcamobony@hotmail.com>
2025-09-11 22:02:07 -0600

    Ejercicio 6

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -118,6 +118,23 @@
         echo "\$c = $c<br>";
     ?>
 
+    <h2>Ejercicio 6</h2>
+    <?php
+        $a = 0;
+        $b = 1;
+        $c = -2;
+        $d = "";
+        $e = "Hola";
+        $f = null;
+
+        var_dump($a); echo "<br>";
+        var_dump($b); echo "<br>";
+        var_dump($c); echo "<br>";
+        var_dump($d); echo "<br>";
+        var_dump($e); echo "<br>";
+        var_dump($f); echo "<br>";
+    ?>
+
 
     
 </body>
